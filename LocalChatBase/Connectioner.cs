2487d822330a9e0ba30b278d74e6c360b4ab5ad9
B0021028 <B0021028@ib.yic.ac.jp>
2023-01-26 12:54:36 +0900

    add connectioner&session

---
+++ LocalChatBase/Connectioner.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalChatBase
+{
+    public class Connectioner
+    {
+
+        public event EventHandler<Session> EvStartSession = (sender, args) => { };
+
+        public static void StartListen()
+        {
+
+        }
+
+        public static Session CreateSession(IPAddress ip, int port)
+        {
+            return new Session(new System.Net.Sockets.TcpClient(ip.ToString(), port));
+        }
+
+
+        public static void StopListen()
+        {
+
+        }
+    }
+}
---
+++ LocalChatBase/Session.cs
@@ -0,0 +1,45 @@
+﻿namespace LocalChatBase
+{
+    public class Session
+    {
+        public event EventHandler<System.Net.IPEndPoint> EvEndSession = (sender, args) => { };
+        public event EventHandler<string> EvReception = (sender, args) => { };
+
+
+        public System.Net.IPEndPoint remoteEndPoint { get; init; }
+
+
+        // インスタンス作成
+        public Session(System.Net.Sockets.TcpClient client)
+        {
+
+        }
+
+        /// <summary>
+        /// セッションを切断します
+        /// </summary>
+        public void EndSession()
+        {
+
+        }
+
+            /// <summary>
+            /// 受信を開始します
+            /// </summary>
+            public void StartReception()
+        {
+        }
+
+        /// <summary>
+        /// テキストデータを送信します
+        /// </summary>
+        /// <param name="data"></param>
+        public void SendData(string data)
+        {
+        }
+
+
+
+    }
+
+}
