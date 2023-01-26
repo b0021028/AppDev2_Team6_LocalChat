a66ef68d756ad9ea7cc6d2b3c45d160131672058
B0021028 <B0021028@ib.yic.ac.jp>
2023-01-26 14:53:56 +0900

    add comment

--- LocalChatBase/Connectioner.cs
+++ LocalChatBase/Connectioner.cs
@@ -7,25 +7,53 @@ using System.Threading.Tasks;
 
 namespace LocalChatBase
 {
+    /// <summary>
+    /// セッションを生成するクラス
+    /// </summary>
     public class Connectioner
     {
+        /// <summary>
+        /// セッションが生まれた時に発火する
+        /// </summary>
+        public static event EventHandler<Session> EvStartSession = (sender, args) => { };
 
-        public event EventHandler<Session> EvStartSession = (sender, args) => { };
 
+        /// 止めるフラグ
+        private static bool s_run = false;
+
+        /// <summary>
+        /// クライアント待ち受けを開始する (ip=any, port=6228)
+        /// </summary>
         public static void StartListen()
         {
-
+            s_run = true;
+            while (s_run)
+            {
+                var session = new Session(new System.Net.Sockets.TcpClient());
+                EvStartSession(null, session);
+            }
         }
 
+
+        /// <summary>
+        /// サーバとのコネクションを行う
+        /// </summary>
+        /// <param name="ip">相手のIPアドレス</param>
+        /// <param name="port">相手のポート番号</param>
+        /// <returns>Sessionのインスタンス</returns>
         public static Session CreateSession(IPAddress ip, int port)
         {
             return new Session(new System.Net.Sockets.TcpClient(ip.ToString(), port));
         }
 
 
-        public static void StopListen()
+        /// <summary>
+        /// クライアント待ち受けを終了しようとする
+        /// </summary>
+        async public static void StopListen()
         {
-
+            s_run = false;
         }
+
     }
 }
