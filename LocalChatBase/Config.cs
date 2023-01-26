847751f46cdffb1cbab44468444ed316e1d2e604
ikunaka ryo <B002101@ib.yic.ac.jp>
2023-01-26 14:51:52 +0900

    a

---
+++ LocalChatBase/Config.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalChatBase
+{
+    class Config
+    {
+        public void OutputConfigFile()
+        {
+        }
+        public void LoadConfigFile()
+        {
+        }
+        public void GetConfig()
+        {
+        }
+
+        public void DetailConfig()
+        {
+        }
+        public void GetDefaultConfig()
+        {
+        }
+        public void DefaultConfig()
+        {
+        }
+    }
+}
