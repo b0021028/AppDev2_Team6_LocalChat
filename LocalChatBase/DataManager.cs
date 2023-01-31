7508d8f0da7aec06308e1560ab277c0491a1e75a
B0021028 <B0021028@ib.yic.ac.jp>
2023-01-31 17:55:02 +0900

    fix adddata

--- LocalChatBase/DataManager.cs
+++ LocalChatBase/DataManager.cs
@@ -1,14 +1,28 @@
 using Org.BouncyCastle.Tls;
+using Org.BouncyCastle.Utilities.Net;
 using System;
 using System.Data;
 using System.Data.SQLite;
-using System.Security.Cryptography;
 
 namespace LocalChatBase
 {
+    public struct Data
+    {
+        string ip;
+        string reception;
+        string time;
+        string message;
+    }
     public class DataManager
     {
         /// <summary>
+        /// データを追加した際に発火します
+        /// </summary>
+        public static event EventHandler<bool> EvAddData = (sender, args) => { };
+
+        private static string s_dataSource { get; } = "Data Source=temptable";
+
+        /*// <summary> よくわからない
         /// データベースの作成とテーブルの作成
         /// </summary>
         static void Main()
@@ -20,7 +34,7 @@ namespace LocalChatBase
 
                 using (var cmd = new SQLiteCommand(cn))
                 {
-                    cmd.CommandText = "CREATE [TEMP | TEMPORARY] TABLE temptable (" +
+                    cmd.CommandText = "CREATE TEMPORARY TABLE temptable (" +
                 "ReceiveFlag NUMERIC NOT NULL," +
                 "Recipient TEXT NOT NULL, " +
                 "Time NUMERIC NOT NULL," +
@@ -28,44 +42,32 @@ namespace LocalChatBase
                     cmd.ExecuteNonQuery();
                 }
             }
-        }
-
-        /// <summary>
-        /// データを追加した際に発火します
-        /// </summary>
-        public static event EventHandler<string> EvAddData = (sender, args) => { };
+        }*/
 
         /// <summary>
         /// データの追加をします 順番：受信フラグ、受け取り人、時間、メッセージ
         /// </summary>
-        
-        public static void AddData(object sender, EventArgs e, string IP, string Reception, string time, string Message)
+        public static void AddData(IPAddress ip, bool reception, DateTime time, string message)
         {
-            SQLiteConnection con = new SQLiteConnection("Data Source=temptable;");
-
-            con.Open();
-            // データの追加を試みる
-            try
+            using (var conn = new SQLiteConnection(s_dataSource))
             {
-                string sql = $"insert into temptable (RaceiveFlag, Recipient, Time, Message) values ({IP}, {Reception}, {time}, {Message});";
-                SQLiteCommand com = new SQLiteCommand(sql, con);
-                com.ExecuteNonQuery();
+                conn.Open();
+                // データの追加を試みる
+                using (SQLiteTransaction sqlt = conn.BeginTransaction())
+                {
+                    var cmd = conn.CreateCommand();
+                    cmd.CommandText = "insert into temptable (RaceiveFlag, Recipient, Time, Message) values (@reception, @ip, @time, @message);";
+                    cmd.Parameters.AddWithValue("@reception", reception);
+                    cmd.Parameters.AddWithValue("@ip", ip);
+                    cmd.Parameters.AddWithValue("@time", time);
+                    cmd.Parameters.AddWithValue("@message", message);
+                    cmd.ExecuteNonQuery();
 
-                EvAddData(null, Reception);
+                    EvAddData(null, reception);
+                    sqlt.Commit();
+                }
+                conn.Close();
             }
-            catch
-            {
-
-            }
-
-            finally
-            {
-                con.Close();
-            }
-            
-
-
-
         }
 
         /// <summary>
@@ -83,9 +85,31 @@ namespace LocalChatBase
         /// <summary>
         /// データを取得します
         /// </summary>
-        public static void GetDatas(string IP)
+        /// <param name="ip"></param>
+        /// <returns></returns>
+        async public static IAsyncEnumerable<object> GetDatas(IPAddress ip)
         {
+            SQLiteDataReader reader;
 
+            using (var conn = new SQLiteConnection(s_dataSource))
+            {
+                conn.Open();
+                var command = conn.CreateCommand();
+                command.CommandText =
+                    $"SELECT RaceiveFlag, Recipient, Time, Message FROM TEMPTABLE " +
+                    "WHERE IP='{ip.ToString()}';";
+                // データの取得
+                reader = command.ExecuteReader();
+
+            }
+            foreach(var data in reader)
+            {
+                yield data;
+            }
+
+
+
+            return new Data();
         }
 
     }
