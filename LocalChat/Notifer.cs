b617eb191a71b7dd834de0b5832dd6421a42817d
ikunaka ryo <B002101@ib.yic.ac.jp>
2023-01-31 15:05:10 +0900

    通知

---
+++ LocalChat/Notifer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalChat
+{
+    public partial class FormMain : FormMain
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.BalloonTipTitle = "おしらせ";
+            notifyIcon1.BalloonTipText = "おしらせのメッセージ";
+            notifyIcon1.ShowBalloonTip(3000);
+        }
+        class Notifer
+        {
+        }
+    }
+}
